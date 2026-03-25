574aaa83a629afbf50d766b4f0faef690422a471
Artem Kutsan <artemkutsan@gmail.com>
2026-03-25 18:47:35 +0100

    chore: изменен заголовок

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
   return (
     <AppBar position="static" sx={{ boxShadow: '0 4px 16px rgba(0, 0, 0, 0.125)', py: 2 }}>
       <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', gap: 2 }}>
-        <Typography variant="h6">Сникер-магазин</Typography>
+        <Typography variant="h6">Магазин кроссовок</Typography>
         <Nav />
       </Toolbar>
     </AppBar>
