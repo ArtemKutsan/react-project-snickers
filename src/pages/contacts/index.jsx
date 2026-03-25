e6876861b4f8b491c72eaebd5a31e00f82bb31bd
Artem Kutsan <artemkutsan@gmail.com>
2026-03-25 17:16:02 +0100

    feat: страница /contacts

--- src/pages/contacts/index.jsx
+++ src/pages/contacts/index.jsx
@@ -1,25 +1,127 @@
 import Meta from '../../components/Meta';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import SnapchatIcon from '@mui/icons-material/Chat';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import XIcon from '@mui/icons-material/X';
 
-const ContactsPage = () => {
+function ContactsPage() {
   return (
     <>
       <Meta title="Контакты" />
 
-      <div className="container max-w-3xl prose">
-        <h1>Контакты</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis culpa doloribus
-          repellendus placeat praesentium beatae dolores consequatur nisi velit reprehenderit.
-        </p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla voluptatum repellat quam
-          quaerat voluptas distinctio dolorem repellendus voluptatem, ab error aperiam eveniet
-          veritatis, accusamus vitae! Reprehenderit quaerat sapiente vitae hic.
-        </p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam, consequatur?</p>
-      </div>
+      <Typography variant="h4" sx={{ pb: 2, mb: 4, borderBottom: '1px solid lightgray' }}>
+        Контакты
+      </Typography>
+
+      <Box sx={{ mt: 8 }}>
+        <Typography color="text.secondary" gutterBottom>
+          8 800 000 00 00
+        </Typography>
+        <Typography color="text.secondary" gutterBottom>
+          emailexample@email.com
+        </Typography>
+      </Box>
+
+      <Box sx={{ display: 'flex', gap: 4, mt: 6 }}>
+        <Box sx={{ flex: 2 }}>
+          <Box sx={{ display: 'flex', gap: 2, mb: 2 }}>
+            <TextField
+              fullWidth
+              placeholder="Ваш email"
+              sx={{
+                '& .MuiOutlinedInput-root': {
+                  bgcolor: '#FAFAFA',
+                  borderRadius: 3,
+                  maxHeight: 40,
+                },
+              }}
+            />
+            <TextField
+              fullWidth
+              placeholder="Ваше имя"
+              variant="outlined"
+              sx={{
+                '& .MuiOutlinedInput-root': {
+                  bgcolor: '#FAFAFA',
+                  borderRadius: 3,
+                  maxHeight: 40,
+                },
+              }}
+            />
+          </Box>
+
+          <TextField
+            fullWidth
+            placeholder="Введите сообщение"
+            multiline
+            rows={4}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                bgcolor: '#FAFAFA',
+                borderRadius: 3,
+              },
+            }}
+          />
+        </Box>
+
+        <Box
+          sx={{
+            flex: 1,
+            bgcolor: '#FAFAFA',
+            borderRadius: 6,
+            p: 4,
+            textAlign: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+          }}
+        >
+          <Typography color="text.secondary" gutterBottom>
+            Найдите нас:
+          </Typography>
+
+          <Box sx={{ display: 'flex', justifyContent: 'center', gap: 1, mt: 2 }}>
+            <IconButton
+              sx={{ bgcolor: '#FFFC00', color: '#000', '&:hover': { bgcolor: '#FFFC00' } }}
+            >
+              <SnapchatIcon />
+            </IconButton>
+
+            <IconButton
+              sx={{ bgcolor: '#1877F2', color: '#fff', '&:hover': { bgcolor: '#1877F2' } }}
+            >
+              <FacebookIcon />
+            </IconButton>
+
+            <IconButton sx={{ bgcolor: '#000', color: '#fff', '&:hover': { bgcolor: '#000' } }}>
+              <XIcon />
+            </IconButton>
+          </Box>
+        </Box>
+      </Box>
+
+      <Box sx={{ textAlign: 'center', mt: 2 }}>
+        <Button
+          variant="contained"
+          sx={{
+            bgcolor: '#090D1A',
+            color: '#fff',
+            borderRadius: 3,
+            '&:hover': {
+              bgcolor: '#090D1A',
+            },
+          }}
+        >
+          Отправить
+        </Button>
+      </Box>
     </>
   );
-};
+}
 
 export default ContactsPage;
